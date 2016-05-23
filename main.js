c51765238bb7bb6b154d843a6edf2166cb9fc97b
AS <sarmasika@gmail.com>
2016-05-23 01:21:56 -0700

    test
    test

---
+++ main.js
@@ -0,0 +1,18 @@
+function UserInfoCtrl($scope, $http) {
+    $scope.getUserInfo = function() {
+        $scope.userNotFound = false;
+        $scope.loaded = false;
+        $http.get("https://api.github.com/users/" + $scope.username)
+            .success(function(data) {
+                $scope.user = data;
+                $scope.loaded = true;
+            })
+            .error(function() {
+                $scope.userNotFound = true;
+            });
+        $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function(data) {
+            $scope.repos = data;
+            $scope.reposFound = data.length;
+        });
+    }
+}
